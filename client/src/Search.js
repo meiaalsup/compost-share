f00b2e5b429f3ffcb59ebf66398d55c21569c235
Meena Rajan <meenar@mit.edu>
2020-05-02 18:40:24 -0700

    attempt at alert

--- client/src/Search.js
+++ client/src/Search.js
@@ -44,6 +44,13 @@ class Search extends React.Component {
   search() {
     let state = this.updateState()
     // TODO: if nothing checked, add alert 
+    let checkedSomething = false;
+    for (const [key, value] of Object.entries(state.availability)) {
+      if (value) {
+	checkedSomething = true;
+      }
+    }
+    if (checkedSomething) {
     let json = JSON.stringify(state)
     console.log(json)
     fetch("http://localhost:3000/search", {
@@ -62,6 +69,8 @@ class Search extends React.Component {
       .catch(e => {
         console.log('There has been a problem with your fetch operation: ' + e.message);
       });
+     } else {
+     }
   }
 
   onChangeHandler(cb) {
@@ -125,11 +134,11 @@ class Search extends React.Component {
           <subheading className="Search-day">
             <t1>Wed:</t1>
           </subheading>
-          <input type="checkbox" id="search_wed_morn" name="search_wed_morn"/>
+          <input type="checkbox" id="search_wed_morn" name="search_wed_morn" />
           <label htmlFor="search_wed_morn">8am-12pm </label>
-          <input type="checkbox" id="search_wed_aft" name="search_wed_aft"/>
+          <input type="checkbox" id="search_wed_aft" name="search_wed_aft" />
           <label htmlFor="search_wed_aft">12pm-4pm </label>
-          <input type="checkbox" id="search_wed_eve" name="search_wed_eve"/>
+          <input type="checkbox" id="search_wed_eve" name="search_wed_eve" />
           <label htmlFor="search_wed_eve">4pm-8pm </label>
           </div>
         </div>
@@ -138,11 +147,11 @@ class Search extends React.Component {
         <subheading className="Search-day">
           <t1>Thu:</t1>
         </subheading>
-        <input type="checkbox" id="search_thurs_morn" name="search_thurs_morn"/>
+        <input type="checkbox" id="search_thurs_morn" name="search_thurs_morn" />
         <label htmlFor="search_thurs_morn">8am-12pm </label>
-        <input type="checkbox" id="search_thurs_aft" name="search_thurs_aft"/>
+        <input type="checkbox" id="search_thurs_aft" name="search_thurs_aft" />
         <label htmlFor="search_thurs_aft">12pm-4pm </label>
-        <input type="checkbox" id="search_thurs_eve" name="search_thurs_eve"/>
+        <input type="checkbox" id="search_thurs_eve" name="search_thurs_eve" />
         <label htmlFor="search_thurs_eve">4pm-8pm </label>
         </div>
       </div>
@@ -151,11 +160,11 @@ class Search extends React.Component {
         <subheading className="Search-day">
           <t1>Fri:</t1>
         </subheading>
-        <input type="checkbox" id="search_fri_morn" name="search_fri_morn"/>
+        <input type="checkbox" id="search_fri_morn" name="search_fri_morn" />
         <label htmlFor="search_fri_morn">8am-12pm </label>
-        <input type="checkbox" id="search_fri_aft" name="search_fri_aft"/>
+        <input type="checkbox" id="search_fri_aft" name="search_fri_aft" />
         <label htmlFor="search_fri_aft">12pm-4pm </label>
-        <input type="checkbox" id="search_fri_eve" name="search_fri_eve"/>
+        <input type="checkbox" id="search_fri_eve" name="search_fri_eve" />
         <label htmlFor="search_fri_eve">4pm-8pm </label>
         </div>
       </div>
@@ -164,11 +173,11 @@ class Search extends React.Component {
         <subheading className="Search-day">
           <t1>Sat:</t1>
         </subheading>
-        <input type="checkbox" id="search_sat_morn" name="search_sat_morn"/>
+        <input type="checkbox" id="search_sat_morn" name="search_sat_morn" />
         <label htmlFor="search_sat_morn">8am-12pm </label>
-        <input type="checkbox" id="search_sat_aft" name="search_sat_aft"/>
+        <input type="checkbox" id="search_sat_aft" name="search_sat_aft" />
         <label htmlFor="search_sat_aft">12pm-4pm </label>
-        <input type="checkbox" id="search_sat_eve" name="search_sat_eve"/>
+        <input type="checkbox" id="search_sat_eve" name="search_sat_eve" />
         <label htmlFor="search_sat_eve">4pm-8pm </label>
         </div>
       </div>
@@ -177,11 +186,11 @@ class Search extends React.Component {
         <subheading className="Search-day">
           <t1>Sun:</t1>
         </subheading>
-        <input type="checkbox" id="search_sun_morn" name="search_sun_morn"/>
+        <input type="checkbox" id="search_sun_morn" name="search_sun_morn" />
         <label htmlFor="search_sun_morn">8am-12pm </label>
-        <input type="checkbox" id="search_sun_aft" name="search_sun_aft"/>
+        <input type="checkbox" id="search_sun_aft" name="search_sun_aft" />
         <label htmlFor="search_sun_aft">12pm-4pm </label>
-        <input type="checkbox" id="search_sun_eve" name="search_sun_eve"/>
+        <input type="checkbox" id="search_sun_eve" name="search_sun_eve" />
         <label htmlFor="search_sun_eve">4pm-8pm </label>
         </div>	
       </div>
