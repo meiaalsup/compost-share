2476ec5cdab06e2d945cacd2e09ed5fee2d30a7d
Meia <meia.alsup@gmail.com>
2020-05-02 21:01:44 -0400

    configure port depending on environment

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors') // enables everyone!!!
-
-
 
 var config = require('./config')
 
